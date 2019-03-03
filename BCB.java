43219ae11c3ce41191f9503de97f85076ca4ba66
salimsoa77 <48161249+salimsoa77@users.noreply.github.com>
2019-03-02 23:44:45 -0500

    uploaded new file

---
+++ BCB.java
@@ -0,0 +1,14 @@
+package InterfaceClass;
+
+public interface BCB {
+
+	int empssn = 178544;
+
+	public void homeLoan();
+
+	public void educationLoan();
+
+	public void carLoan();
+
+}
+// by default interface is public and abstract
